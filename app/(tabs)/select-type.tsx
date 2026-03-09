70046d5ec3bf32759b78150f5cf153bbfdefb456
7ewon <aewn6294@gmail.com>
2026-03-09 21:14:09 +0900

    Chore: select-type 화면에 지도 이미지 추가

--- app/(tabs)/select-type.tsx
+++ app/(tabs)/select-type.tsx
@@ -2,7 +2,7 @@ import BoxCard from "@/components/BoxCard";
 import ButtonBack from "@/components/ButtonBack";
 import ButtonPrimary from "@/components/ButtonPrimary";
 import { useRouter } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 type Tag =
   | "phone"
@@ -28,7 +28,7 @@ export default function LostItemPage() {
     { image: require('@/assets/images/watch.png'), label: "시계" },
     { image: require('@/assets/images/clothes.png'), label: "옷" },
     { image: require('@/assets/images/more.png'), label: "기타" },
-];
+  ];
 
   const handleSelect = (tag: Tag) => {
     router.push({
@@ -38,42 +38,67 @@ export default function LostItemPage() {
   };
 
   return (
-    <View className="flex-1 bg-gray-100 px-8">
-      <View className="w-full h-1 bg-gray-200 mt-20 overflow-hidden">
-              <View className="w-2/3 h-1 bg-black" />
+    <View className="flex-1 bg-gray-100">
+
+      <Image
+      source={require('@/assets/images/map-image.png')}
+      style={{
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        height: 280,
+      }}
+    />
+
+    <Image
+      source={require('@/assets/images/gradient.png')}
+      style={{
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        height: 280,
+      }}
+    />
+      <View className="px-8 flex-1">
+        <View className="w-full h-1 bg-gray-200 mt-20 overflow-hidden">
+          <View className="w-2/3 h-1 bg-black" />
+        </View>
+
+        <ButtonBack />
+
+        <View className="flex-1 justify-center">
+          <Text className="text-[24px] font-pretendard-bold mb-8 px-4">
+            잃어버린 물건이 무엇인가요?
+          </Text>
+
+          <View className="flex-row flex-wrap">
+            {items.map((item, index) => (
+              <View key={index} className="w-1/3 p-2">
+                <BoxCard
+                  image={item.image}
+                  label={item.label}
+                  onPress={() => handleSelect(item.label as Tag)}
+                />
+              </View>
+            ))}
           </View>
-          <ButtonBack />
-      <View className="flex-1 justify-center">
-        <Text className="text-[24px] font-pretendard-bold mb-8 px-4">
-          잃어버린 물건이 무엇인가요?
-        </Text>
 
-        <View className="flex-row flex-wrap">
-          {items.map((item, index) => (
-            <View key={index} className="w-1/3 p-2">
-              <BoxCard
-                image={item.image}
-                label={item.label}
-                onPress={() => handleSelect(item.label as Tag)} 
-              />
-            </View>
-          ))}
+          <Text className="text-center text-[20px] font-pretendard text-gray-400 my-12">
+            또는?
+          </Text>
+
+          <ButtonPrimary
+            title="지도에서 전체 보기"
+            onPress={() =>
+              router.push({
+                pathname: "/map",
+                params: { tag: "all" },
+              })
+            }
+          />
+        </View>
       </View>
 
-      <Text className="text-center text-[20px] font-pretendard text-gray-400 my-12">
-        또는?
-      </Text>
-
-      <ButtonPrimary
-        title="지도에서 전체 보기"
-        onPress={() =>
-          router.push({
-            pathname: "/map",
-            params: { tag: "all" },
-          })
-        }
-      />
-      </View>
     </View>
   );
 }
